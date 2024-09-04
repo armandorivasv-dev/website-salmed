efd4a64ed206438b2a74a6617d470574714beaa5
Armando Rivas <armandorivasv.dev@gmail.com>
2024-09-04 11:26:32 -0400

    add vercel page insights

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import getCustomTheme from '@/styles/get-custom-theme';
 import CssBaseline from '@mui/material/CssBaseline';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Layout({ children }) {
   const customTheme = createTheme(getCustomTheme('light'));
@@ -14,6 +15,7 @@ export default function Layout({ children }) {
         <CssBaseline />
         <TopNav mode={'light'} />
         <main>{children}</main>
+        <SpeedInsights />
         <Footer />
       </ThemeProvider>
     </>
